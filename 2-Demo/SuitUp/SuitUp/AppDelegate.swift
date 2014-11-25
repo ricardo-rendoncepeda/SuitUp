22d2335823b1eb3d7689ddea160dc772413e9d5c
Ricardo Rendon Cepeda <ricardo.rendon.cepeda@idean.com>
2014-11-25 10:30:23 -0800

    Added 'Demo Solution'

---
+++ 2-Demo/SuitUp/SuitUp/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  SuitUp
+//
+//  Created by Ricardo on 11/25/14.
+//  Copyright (c) 2014 RayWenderlich. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+
+  func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    return true
+  }
+}
+
---
+++ 2-Demo/SuitUp/SuitUp/LegendaryDeck/CardView.swift
@@ -0,0 +1,12 @@
+//
+//  CardView.swift
+//  SuitUp
+//
+//  Created by Ricardo on 11/25/14.
+//  Copyright (c) 2014 RayWenderlich. All rights reserved.
+//
+
+import UIKit
+
+class CardView: UIView {
+}
