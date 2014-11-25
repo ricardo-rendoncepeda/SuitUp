72f138cc42ab5559922f337d3a31db5ffd367cf7
Ricardo Rendon Cepeda <ricardo.rendon.cepeda@idean.com>
2014-11-25 10:38:31 -0800

    Added 'Lab Solution'

---
+++ 3-Lab/SuitUp/SuitUp/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  SuitUp
+//
+//  Created by Ricardo on 11/25/14.
+//  Copyright (c) 2014 RayWenderlich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
+
